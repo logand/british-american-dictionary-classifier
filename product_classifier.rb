30ca581712a0a254374035ffb9a0a9f1651f5901
Logan Donovan <commit@logandonovan.com>
2017-11-03 13:08:01 -0400

    Pre-generate language dictionaries with plural and past tense words included, match against dictionary

--- product_classifier.rb
+++ product_classifier.rb
@@ -29,9 +29,6 @@ product_classification = { british: [], american: [], mixed: [], unknown: [] }
 
 Linguistics.use( :en )
 
-# american_words_map = american_words.map { |word| [word, true] }.to_h
-# british_words_map = british_words.map { |word| [word, true] }.to_h
-
 HTTP = GraphQL::Client::HTTP.new("https://www.teacherspayteachers.com/graph/graphql")
 
 if (File.exist?("data/schema.json"))
@@ -55,6 +52,17 @@ GRAPHQL
 
 result = Client.query(ProductLookupQuery, variables: { "productIds": product_numbers })
 
+def generate_library(word_list)
+  dict = {}
+  word_list.each do |word|
+    dict[word] = true
+    dict[word.en.plural] = true
+    dict[word.en.past_tense] = true
+  end
+
+  dict
+end
+
 def check_for_dictionary_words(product, dialect_dictionary)
   name = Loofah.fragment(product.name).text
   description = Loofah.fragment(product.description).text
@@ -64,13 +72,11 @@ end
 def check_string(string, dialect_dictionary)
   found_match = false
   string.split(" ").each do |word|
-    dialect_dictionary.each do |comparison_word|
-      if word.include? comparison_word
-        found_match = true
-        break
-      end
-    end
+    word = strip_word(word)
+    found_match = !!dialect_dictionary[word]
+    break if found_match
   end
+
   found_match
 end
 
@@ -85,30 +91,24 @@ end
 
 def check_string_with_matches(string, dialect_dictionary)
   matches = string.split(" ").reduce([]) do |match_arr, word|
+    word = strip_word(word)
+    word_matches = !!dialect_dictionary[word] ? [word] : []
     substring_matches = []
-    word_matches = dialect_dictionary.select do |comparison_word|
+    dialect_dictionary.keys.each do |comparison_word|
                      substring_matches.push(comparison_word) if word.include? comparison_word
-                     compare_strings(word, comparison_word)
                    end
     match_arr.push({ word: word, matches: word_matches, substring_matches: substring_matches }) if word_matches.length > 0 || substring_matches.length > 0
     match_arr
   end
 end
 
-def compare_strings(word, dictionary_word)
-  comparison_word = word.gsub(/[^[:alpha:]]/i, '')
+def word_in_dictionary(word, dictionary)
+  puts "The word was #{comparison_word} and is: #{!!dictionary[comparison_word]}"
 
-  # Match the word, pluralized form, past tense form
-  case comparison_word
-  when dictionary_word
-    true
-  when dictionary_word.en.plural
-    true
-  when dictionary_word.en.past_tense
-    true
-  else
-    false
-  end
+end
+
+def strip_word(word)
+  word.gsub(/[^[:alpha:]]/i, '')
 end
 
 ###
@@ -126,17 +126,30 @@ def classify_product(product, has_american_words, has_british_words, classificat
   end
 end
 
+american_words_map = generate_library(american_words)
+british_words_map = generate_library(british_words)
+
+### Use Verbose Functions
+# result.data.products.each do |product|
+#     american_matches = check_for_dictionary_words_with_matches(product, american_words_map)
+#     match_sum = american_matches.map { |match_obj| match_obj[:matches].length }
+#     has_american_words = match_sum.sum > 0
+#     british_matches = check_for_dictionary_words_with_matches(product, british_words_map)
+#     match_sum = british_matches.map { |match_obj| match_obj[:matches].length }
+#     has_british_words = match_sum.sum > 0
+#     classification = classify_product(product, has_american_words, has_british_words, product_classification)
+#     product_classification[classification.to_sym].push(product.id)
+#     result_classification.push({ id: product.id, american_matches: american_matches, british_matches: british_matches, classification: classification })
+# end
+
+### Use Normal Functions
 result.data.products.each do |product|
-    american_matches = check_for_dictionary_words_with_matches(product, american_words)
-    match_sum = american_matches.map { |match_obj| match_obj[:matches].length }
-    has_american_words = match_sum.sum > 0
-    british_matches = check_for_dictionary_words_with_matches(product, british_words)
-    match_sum = british_matches.map { |match_obj| match_obj[:matches].length }
-    has_british_words = match_sum.sum > 0
+    has_american_words = check_for_dictionary_words(product, american_words_map)
+    has_british_words = check_for_dictionary_words(product, british_words_map)
     classification = classify_product(product, has_american_words, has_british_words, product_classification)
     product_classification[classification.to_sym].push(product.id)
-    result_classification.push({ id: product.id, american_matches: american_matches, british_matches: british_matches, classification: classification })
+    result_classification.push({ id: product.id, american_match: has_american_words, british_match: has_british_words, classification: classification })
 end
 
-# puts result_classification
+puts result_classification
 puts "The results of analyzing the words in each product lead us to classify them as follows: #{product_classification}"
